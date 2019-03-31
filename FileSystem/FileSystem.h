0d45aea3f4e4f6343c48d85f4313f2f073c7a670
yun <yhu@gmail.com>
2019-03-31 19:14:02 +0200

    #EDIT: (foundation) Minor progress on the FileSystem

--- FileSystem/FileSystem.h
+++ FileSystem/FileSystem.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 //#include <atomic>
 #include <Windows.h>
+#include <filesystem>
 
 #include <util/Assert.h>
 #include <Container/SimpleFixedQueue.h>
@@ -12,7 +13,6 @@ namespace std
 {
 template <typename t_Type> struct atomic;
 }
-
 namespace Foundation
 {
 namespace FiberSystem
@@ -27,42 +27,66 @@ struct FileRequestDecl
   std::atomic<int32_t>* counter;
   const char* filePath;
   void* data;
+
+  uint64_t sizeInBytes;
 };
-// TOOD: Make this File system fiber specific?
 //-----------------------------------------------------------------------------
 template <typename t_Allocator> struct FileSystem
 {
+  using FileSystemQueue =
+      Container::SimpleFixedQueue<t_Allocator, FileRequestDecl, 128u>;
   // TODO: Create event
   static void init()
   {
   }
 
-  static void enqueueLoadRequest()
+  static void enqueueLoadRequest(FileRequestDecl* p_FiberRequestDecl,
+                                 uint32_t p_Count)
   {
+    Foundation::ScopedGuard(ms_QueueMutex);
+    for (uint32_t i = 0u; i < p_Count; i++)
+    {
+      ms_Queue.enqueueBack(p_FiberRequestDecl[p_Count]);
+    }
   }
 
   // Processes load requests; if any, else sleep till event gets called
-  static void processLoadRequestAndSleep(FileRequestDecl* p_FiberRequestDecl,
-                                         uint32_t p_Count)
+  static void processLoadRequestAndSleep()
   {
-    for (uint32_t i = 0u; i < p_Count; i++)
-    {
-    }
+    processLoadRequests();
+
     // TOOD: Let the user handle the loop instead?
     // spinlock for a while first
   }
 
   static void processLoadRequests()
   {
+    {
+      Foundation::ScopedGuard(ms_QueueMutex);
+      ms_ProcessQueue.clear();
+      // Copy queue
+      Container::copy(ms_ProcessQueue, ms_Queue);
+      ms_Queue.clear();
+    }
+
+    FileRequestDecl fberRequestDecl;
+    while (ms_ProcessQueue.dequeueFront(&fberRequestDecl))
+    {
+      // load the file here
+    }
   }
 
 private:
-  static void _dequeueLoadRequest(){
-
-  };
+  static void _fileExist(){};
 
   // TODO: hardcoded queue Size
-  Containers::SimpleFixedQueue<t_Allocator, FileRequestDecl, 1000u> m_Queue;
+  static FileSystemQueue ms_ProcessQueue;
+  static FileSystemQueue ms_Queue;
+  static Foundation::Mutex ms_QueueMutex;
+
+  // TODO: find a better place for this
+  const uint64_t RingBufferSize = 1024u * 1024u * 10u;
+  static Foundation::Memory::LinearAllocator<true> ms_RingBuffer;
 };
 //-----------------------------------------------------------------------------
 }; // namespace FiberSystem
