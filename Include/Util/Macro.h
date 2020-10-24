ddef99279ef31c02ae49b1791caf8cac5aab604d
yunmnmn <yunyghu@gmail.com>
2020-10-25 01:27:18 +0900

    #EDIT: add UNUSED macro

--- Include/Util/Macro.h
+++ Include/Util/Macro.h
@@ -3,3 +3,5 @@
 #define NO_COPY_ASSIGN(_Class)                                                                                                     \
    _Class(const _Class&) = delete;                                                                                                 \
    _Class& operator=(const _Class&) = delete
+
+#define UNUSED(_VARIABLE) (void)_VARIABLE
