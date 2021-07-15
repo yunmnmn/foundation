f3287099347f126154ea89cbf421886640226ddb
yunmnmn <yunyghu@gmail.com>
2021-07-16 03:52:18 +0900

    #EDIT: wstring_bootstrap

--- Include/Std/string_bootstrap.h
+++ Include/Std/string_bootstrap.h
@@ -8,5 +8,6 @@ namespace Foundation
 namespace Std
 {
 using string_bootstrap = eastl::basic_string<char, Memory::EastlBootstrapAllocator>;
-};
+using wstring_bootstrap = eastl::basic_string<wchar_t, Memory::EastlBootstrapAllocator>;
+}; // namespace Std
 }; // namespace Foundation
